3345f343735e8a91556af888744df314f22862ea
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-12-17 22:49:55 +0600

    feat: base Home page components setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import FAQ from "../components/FAQ.tsx";
+import Hero from "../components/Hero.tsx";
+import Benefits from "../components/Benefits.tsx";
+import Courses from "../components/Courses.tsx";
+import Testimonials from "../components/Testimonials.tsx";
+import Pricing from "../components/Pricing.tsx";
+
 const Home = () => {
-  return <div className="border border-red-400">Be Vietnam</div>;
+  return (
+    <div className="border border-red-400">
+      <Hero />
+      <Benefits />
+      <Courses />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+    </div>
+  );
 };
 
 export default Home;
