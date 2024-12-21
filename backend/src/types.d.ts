c5531f2917a7d51421d48ac3cbd9e615fb2ef34d
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-12-21 22:03:24 +0600

    feat: add Plan model. Add fixtures

---
+++ backend/src/types.d.ts
@@ -0,0 +1,25 @@
+import { Model } from "mongoose";
+
+declare type FeatureName =
+  | "courseAccess"
+  | "materialsAndResources"
+  | "support"
+  | "certificate"
+  | "adds"
+  | "exclusiveForumAccess"
+  | "earlyAccess";
+
+declare interface IFeatureInfo {
+  value: boolean;
+  title: string;
+}
+
+declare interface IPlan {
+  type: "free" | "paid";
+  name: string;
+  perMonth: number;
+  perYear: number;
+  features: Record<FeatureName, IFeatureInfo>;
+}
+
+declare type PlanModel = Model<IPlan>;
